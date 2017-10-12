08998fe4b14a89a54ad5c59f928aa6ac69b52fd3
Qiulonghui <569374939@qq.com>
2017-10-12 08:47:48 +0800

    20171012

---
+++ pages/goodslist/goodslist.js
@@ -0,0 +1,13 @@
+// pages/goodslist/goodslist.js
+Page({
+  data: {
+    goodslist:{
+      title:"健康家居",
+      itemArr:[
+        { url: "", img: "sp1", mainText: "乐扣", middleText: "玻璃套装保鲜五件套", subText: "呵护您的美丽容颜", score:"2500"},
+        { url: "/pages/goodsdetail/goodsdetail", img: "sp1", mainText: "乐扣", middleText: "玻璃套装保鲜五件套", subText: "伴您环保居家", score: "2500"},
+        { url: "", img: "sp1", mainText: "乐扣", middleText: "玻璃套装保鲜五件套", subText: "让家人的生活更精彩", score: "2500" }
+      ]
+    }
+  },
+})
---
+++ pages/store/store.js
@@ -0,0 +1,12 @@
+// pages/store/store.js
+Page({
+  data: {
+    itemArr:[
+      { url: "", img: "sp1", mainText: "健康护理", subText: "呵护您的美丽容颜" },
+      { url: "/pages/goodslist/goodslist", img: "sp2", mainText: "健康居家", subText: "伴您环保居家" },
+      { url: "", img: "sp3", mainText: "健康生活", subText: "让家人的生活更精彩" },
+      { url: "", img: "sp4", mainText: "健康运动", subText: "有氧运动一起参与" },
+      { url: "", img: "sp5", mainText: "健康公益", subText: "公益基金让世界充满爱" },
+    ]
+  },
+})
